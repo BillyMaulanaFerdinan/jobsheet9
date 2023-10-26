252bc7ddf5c91b1ef2e10a1f94a3636af254b9e8
BillyMaulanaFerdinan <billy.maulana2020@gmail.com>
2023-10-26 15:10:04 +0700

    Rata-rata nilai modif

--- ArrayRataNilai06.java
+++ ArrayRataNilai06.java
@@ -4,7 +4,10 @@ public class ArrayRataNilai06 {
     
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] nilaiMhs = new int [10];
+        int jmlMhs;
+        System.out.print("Masukkan jumlah mahasiswa: ");
+        jmlMhs = sc.nextInt();
+        int[] nilaiMhs = new int [jmlMhs];
         double total = 0;
         double rata2;
         for (int i = 0; i < nilaiMhs.length; i++){
@@ -13,6 +16,14 @@ public class ArrayRataNilai06 {
         }
         for (int i = 0; i < nilaiMhs.length; i++){
             total += nilaiMhs[i];
+        }
+        for (int i = 0; i < nilaiMhs.length; i++){
+            if (nilaiMhs[i] > 70) {
+                System.out.println("Mahasiswa ke-"+i+" lulus!");
+            }
+            else{
+                System.out.println("Mahasiswa ke-"+i+" tidak lulus!");
+            }
         }
         rata2 = total/nilaiMhs.length;
         System.out.println("Rata-rata nilai = "+rata2);
